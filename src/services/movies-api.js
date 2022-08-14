8aa84454b5508b4947b0c507d3d48a11967948f1
Iryna <93982819+IrinaSkrytska@users.noreply.github.com>
2022-08-14 09:16:45 +0300

    Update movies-api.js

--- src/services/movies-api.js
+++ src/services/movies-api.js
@@ -24,7 +24,7 @@ export async function getPopularMovies() {
 export async function searchMovies(query) {
   try {
     const request = await axios.get(
-      `${BASE_URL}search/movie?api_key=${KEY}&language=en-US&query=${query}&page=1&include_adult=true`
+      `https://api.themoviedb.org/3/search/movie?api_key=${KEY}&language=en-US&query=${query}&page=1&include_adult=true`
     );
     const detailedRequest = request.data.results.map(e => {
       return {
